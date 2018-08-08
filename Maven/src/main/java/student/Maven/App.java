e8160511aff1dc5cb7001e6da62002a1c9264041
AZ <AZ@AZ>
2018-08-08 10:26:14 +0530

    Commit1

---
+++ Maven/src/main/java/student/Maven/App.java
@@ -0,0 +1,13 @@
+package student.Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
